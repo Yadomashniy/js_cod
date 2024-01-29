8361a1c4dc22b2de8a51a315398560897519cc66
Yadomashniy <nikita.ushakov.04@gmail.com>
2024-01-29 21:27:17 +0900

    modified index.js

---
+++ 2 Задание/3 задание/index.js
@@ -0,0 +1,17 @@
+let ageofPerson1 = 18
+let ageofPerson2 = '20'
+
+let nameOfAnimall = null
+let nameOfAnimal2 = 'Charlie'
+
+const bestProgrammingLanguagel = 'Javascript'
+const bestprogrammingLanguage2 = 'Java'
+
+let initialValuel
+let initialValue2 = 0
+
+var isJavaScriptProgrammer1 = true
+var isJavascriptProgrammer2 = false
+
+let helloText1 = 'Hello!'
+let helloText2 = 'Привет!'
