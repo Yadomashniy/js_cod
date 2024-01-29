65b8a11f31040bb27b859bb33b03d448fca21ffa
Yadomashniy <nikita.ushakov.04@gmail.com>
2024-01-29 21:56:47 +0900

    modified

---
+++ 3 Задание/1 Задание/index.js
@@ -0,0 +1,15 @@
+let number = 10;
+
+let string = "Hello";
+
+let boolean = true;
+
+let undefinedValue;
+
+let nullValue = null;
+
+let array = [1, 2, 3];
+
+let object = {key: "value"};
+
+let symbol = Symbol("two");
